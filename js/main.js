b7b04e6604d3508cfacd74ea4eca7decce244b67
Anthony Amici <anthony.amici@grayscale.com.hk>
2018-01-18 23:27:46 +0800

    remade selection & sequential adding/removing & result generation

--- js/main.js
+++ js/main.js
@@ -1,68 +1,154 @@
-var ul = document.getElementById('list');
-var liSelected;
-var index = -1;
+var list = document.getElementById('list');
+var activeSelection;
+var activeIndex = 0;
+
+var prepIndex = 0;
+var prepArray = [];
+var prepIndexA;
+var prepIndexB;
+var prepIndexC;
+var prepTotal = 0;
+
+var resultText;
 
 document.addEventListener('keydown', function(event) {
-var len = ul.getElementsByTagName('li').length-1;
-  if(event.which === 40) {
-index++;
-  //down 
-  if (liSelected) {
-			removeClass(liSelected, 'selected');
-      next = ul.getElementsByTagName('li')[index];
-      if(typeof next !== undefined && index <= len) {
-      
-                liSelected = next;
-            } else {
-             	index = 0;
-                 liSelected = ul.getElementsByTagName('li')[0];
-            }
-            addClass(liSelected, 'selected');
-            console.log(index);
-    }
-    else {
-    index = 0;
-    
-   	 liSelected = ul.getElementsByTagName('li')[0];
-			 addClass(liSelected, 'selected');
-    }
-  }
-  else if (event.which === 38) {
   
-  //up
-    if (liSelected) {
-			removeClass(liSelected, 'selected');
-      index--;
-      console.log(index);
-      next = ul.getElementsByTagName('li')[index];
-      if(typeof next !== undefined && index >= 0) {
-                liSelected = next;
-            } else {
-            		index = len;
-                 liSelected = ul.getElementsByTagName('li')[len];
-            }
-            addClass(liSelected, 'selected');
+  /* --- BROWSING ITEMS --- */
+  var len = list.getElementsByTagName('li').length - 1;
+  
+  // Down
+  if (event.which === 40) {
+    if (activeSelection) {
+      activeIndex++;
+      removeClass(activeSelection, 'active');
+      next = list.getElementsByTagName('li')[activeIndex];
+      if (typeof next !== undefined && activeIndex <= len) { // in list
+        activeSelection = next;
+      }
+      else { // bottom end of list
+        activeIndex = 0;
+        activeSelection = list.getElementsByTagName('li')[0];
+      }
+      addClass(activeSelection, 'active');
     }
-    else {
-    index = 0;
-   	 liSelected = ul.getElementsByTagName('li')[len];
-			addClass(liSelected, 'selected');
+    else { // first press only
+      activeIndex = 0;
+      activeSelection = list.getElementsByTagName('li')[0];
+      addClass(activeSelection, 'active');
     }
   }
+  
+  // Up
+  else if (event.which === 38) {
+    if (activeSelection) {
+      activeIndex--;
+			removeClass(activeSelection, 'active');
+      next = list.getElementsByTagName('li')[activeIndex];
+      if (typeof next !== undefined && activeIndex >= 0) { // in list
+        activeSelection = next;
+      }
+      else { // top end of list
+        activeIndex = len;
+        activeSelection = list.getElementsByTagName('li')[len];
+      }
+      addClass(activeSelection, 'active');
+    }
+    else { // first press only
+      activeIndex = len;
+   	  activeSelection = list.getElementsByTagName('li')[len];
+			addClass(activeSelection, 'active');
+    }
+  }
+  
+  /* --- ADDING TO PREP --- */
+  // Enter
+  else if (event.which === 13) {
+    for (var i = 0; i < prepArray.length; i++) {
+      //console.log('array for loop ' + prepArray[i])
+    }
+    
+    if (prepIndex == 0) {
+      //prepArray.push(activeIndex);
+      addClass(activeSelection, 'selected');
+      document.getElementById('s1').innerHTML = cakeList[activeIndex][0];
+      prepTotal += cakeList[activeIndex][1];
+      prepIndex++;
+      prepIndexA = activeIndex;
+    }
+    else if (prepIndex == 1) {
+      if (activeIndex !== prepIndexA) {
+        addClass(activeSelection, 'selected');
+        document.getElementById('s2').innerHTML = cakeList[activeIndex][0];
+        prepTotal += cakeList[activeIndex][1];
+        prepIndex++;
+        prepIndexB = activeIndex;
+      }
+      else {
+        removeClass(activeSelection, 'selected');
+        document.getElementById('s1').innerHTML = '_';
+        prepTotal -= cakeList[activeIndex][1];
+        prepIndex--;
+      }
+    }
+    else if (prepIndex == 2) {
+      if (activeIndex !== prepIndexB) {
+        addClass(activeSelection, 'selected');
+        document.getElementById('s3').innerHTML = cakeList[activeIndex][0];
+        prepTotal += cakeList[activeIndex][1];
+        prepIndex++;
+        prepIndexC = activeIndex;
+      }
+      else {
+        removeClass(activeSelection, 'selected');
+        document.getElementById('s2').innerHTML = '_';
+        prepTotal -= cakeList[activeIndex][1];
+        prepIndex--;
+      } 
+    }
+    else if (prepIndex == 3 && activeIndex == prepIndexC) {
+      removeClass(activeSelection, 'selected');
+      document.getElementById('s3').innerHTML = '_';
+      prepTotal -= cakeList[activeIndex][1];
+      prepIndex--;
+    }
+    console.log('prepTotal: ' + prepTotal)
+  }
+  
+  /* --- BAKING RESULT --- */
+  // Y
+  else if (event.which === 89 && prepIndex == 3) {
+    if (prepTotal <= 2) {
+      resultText = 'Disgusting!';
+    }
+    else if (prepTotal >= 5) {
+      resultText = 'Amazing!';
+    }
+    else {
+      resultText = 'Ehhcaptable.'
+    }
+    document.getElementById('result').innerHTML = resultText;
+  } 
 }, false);
 
+function updateHTML() {
+  
+}
+
+/* --- ADD/REMOVE CLASSES --- */
 function removeClass(el, className) {
-    if(el.classList) {
-        el.classList.remove(className);
-    } else {
-        el.className = el.className.replace(new RegExp('(^|\\b)' + className.split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
-    }
+  if(el.classList) {
+    el.classList.remove(className);
+  }
+  else {
+    el.className = el.className.replace(new RegExp('(^|\\b)' + className.split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
+  }
 };
 
 function addClass(el, className) {
-    if(el.classList) {
-        el.classList.add(className);
-    } else {
-        el.className += ' ' + className;
-    }
+  if(el.classList) {
+    el.classList.add(className);
+  }
+  else {
+    el.className += ' ' + className;
+  }
 };
